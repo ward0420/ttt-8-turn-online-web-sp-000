30c3095186324af9377bb64691401632c76bc109
Don Ward <10is0420@gmail.com>
2019-03-14 01:52:54 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -10,11 +10,3 @@ def input_to_index(user_input)
   converted_input = user_input.to_i - 1
 end
 
-def move (board, position, character = "X")
-    board[position] = character
-end
-
-#def valid_move?(board,index)
- # return true if position_taken?(board, index) == false && index.between?(0, 8)
-  #else return false if position_taken?(board, index) == true && index.between?(0,8)
-#end
