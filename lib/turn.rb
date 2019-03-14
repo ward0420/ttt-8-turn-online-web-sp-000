7206d3a164215fe9117961ce697af0ba5d258207
Don Ward <10is0420@gmail.com>
2019-03-14 01:54:45 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -14,3 +14,8 @@ def valid_move?(board,index)
   return true if position_taken?(board, index) == false && index.between?(0, 8)
   else return false if position_taken?(board, index) == true && index.between?(0,8)
 end
+
+def position_taken?(board, index)
+  return false if board[index] == " " || board[index] == "" || board[index] == nil
+  else return true
+end
